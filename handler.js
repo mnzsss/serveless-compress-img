03c1d43eeebfa6bfe36077a0c0a15fed2683c56c
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2021-01-05 14:08:03 -0300

    initial commit

---
+++ handler.js
@@ -0,0 +1,11 @@
+module.exports.hello = async (event) => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    },
+    null,
+    2,
+  ),
+});
